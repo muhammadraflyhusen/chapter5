0edc8dc1349d61c3033ced596d7997b86aa237c1
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-03-26 15:19:44 +0700

    inputdata

--- fungsiswicth.cpp
+++ fungsiswicth.cpp
@@ -1,5 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int bilangan1, bilangan2;
+
+void inputData() 
+{
+    cout << "Masukan bilangan pertama : ";
+    cin >> bilangan1;
+    cout << "Masukan bilangan kedua : ";
+    cin >> bilangan2;
+}
+
 
